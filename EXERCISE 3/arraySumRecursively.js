dec13219f881946fb4fe42f6ca56b5875cb3b9e8
ajang20 <ajangkorenilo8@gmail.com>
2026-04-19 16:02:32 +0200

    A function that finds the sum of array numbers recursively

--- EXERCISE 3/arraySumRecursively.js
+++ EXERCISE 3/arraySumRecursively.js
@@ -1,4 +1,12 @@
-
 function arraySumRecursively(array){
+    if(!Array.isArray(array)) return 'Enter array of numbers'
+    if(array.length ===0 ) return 0;
     
+const arr = array.splice(0,1)
+return Number(arr) + arraySumRecursively(array)
 }
+
+// test cases 
+console.log(arraySumRecursively([1,2,3,6]))
+console.log(arraySumRecursively([1,2,4,4]))
+console.log(arraySumRecursively(null))
