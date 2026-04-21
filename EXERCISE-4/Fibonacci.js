b1bdb1c0bdb9c5fed958f6a044fce5aa874732c9
ajang20 <ajangkorenilo8@gmail.com>
2026-04-21 15:12:48 +0200

    function that finds the fibonacci value

--- EXERCISE-4/Fibonacci.js
+++ EXERCISE-4/Fibonacci.js
@@ -1,13 +1,13 @@
-function Fibonacci(n) {
-  let [a, b] = [0, 1];
-
-  for (let i = 0; i < n; i++) {
-    let next = a + b;
-    a = b;
-    b = next;
-  }
-
-  return a;
+function Fibonacci(n){
+   let [a,b] = [0,1]
+   
+  for(let i=0;i<n;i++){
+      let next = a + b
+       a = b;
+       b = next;
+   }
+   
+  return  a
 }
 
-console.log(Fibonacci(6));
+console.log(Fibonacci(6))
